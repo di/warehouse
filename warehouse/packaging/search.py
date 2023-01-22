cafc96a13196f6a12886c59bc44db05f2ea75dd9
Dustin Ingram <di@users.noreply.github.com>
2023-01-22 00:36:45 +0000

    Adopt packaging_legacy

--- warehouse/packaging/search.py
+++ warehouse/packaging/search.py
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import packaging.version
+import packaging_legacy.version
 
 from elasticsearch_dsl import Date, Document, Float, Keyword, Text, analyzer
 
@@ -57,7 +57,9 @@ class Project(Document):
         obj["name"] = release.name
         obj["normalized_name"] = release.normalized_name
         obj["version"] = sorted(
-            release.all_versions, key=lambda r: packaging.version.parse(r), reverse=True
+            release.all_versions,
+            key=lambda r: packaging_legacy.version.parse(r),
+            reverse=True,
         )
         obj["latest_version"] = release.latest_version
         obj["summary"] = release.summary
--- warehouse/packaging/utils.py
+++ warehouse/packaging/utils.py
@@ -14,7 +14,7 @@ import hashlib
 import os.path
 import tempfile
 
-from packaging.version import parse
+import packaging_legacy.version
 from pyramid_jinja2 import IJinja2Environment
 from sqlalchemy.orm import joinedload
 
@@ -46,7 +46,7 @@ def _simple_detail(project, request):
         .join(Release)
         .filter(Release.project == project)
         .all(),
-        key=lambda f: (parse(f.release.version), f.filename),
+        key=lambda f: (packaging_legacy.version.parse(f.release.version), f.filename),
     )
 
     return {
