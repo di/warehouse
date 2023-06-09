1a17ec803c26f134743f697087b8b1da8f494e11
Dustin Ingram <di@users.noreply.github.com>
2023-06-09 13:48:40 +0000

    fixup minor refactoring

--- warehouse/oidc/forms/__init__.py
+++ warehouse/oidc/forms/__init__.py
@@ -15,7 +15,6 @@ from warehouse.oidc.forms._core import DeletePublisherForm
 from warehouse.oidc.forms.github import GitHubPublisherForm, PendingGitHubPublisherForm
 from warehouse.oidc.forms.google import GooglePublisherForm, PendingGooglePublisherForm
 
-
 __all__ = [
     "DeletePublisherForm",
     "GitHubPublisherForm",
