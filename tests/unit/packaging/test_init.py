0f7ee4a79be7b9583446f0288355a2977e5d06ea
Dustin Ingram <di@users.noreply.github.com>
2026-02-18 19:58:26 +0000

    Support automatic deletion of releases for nightlies

--- tests/unit/packaging/test_init.py
+++ tests/unit/packaging/test_init.py
@@ -6,7 +6,6 @@ from celery.schedules import crontab
 
 from warehouse import packaging
 from warehouse.accounts.models import Email, User
-from warehouse.manage.tasks import update_role_invitation_status
 from warehouse.organizations.models import Organization
 from warehouse.packaging.interfaces import (
     IDocsStorage,
@@ -160,9 +159,5 @@ def test_includeme(monkeypatch):
     )
     assert (
         pretend.call(crontab(minute="*/5"), update_description_html)
-        in config.add_periodic_task.calls
-    )
-    assert (
-        pretend.call(crontab(minute="*/5"), update_role_invitation_status)
         in config.add_periodic_task.calls
     )
